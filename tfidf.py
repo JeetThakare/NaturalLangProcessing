9e862bb9bece89fcecd8e9b0410d069f22624c93
Pranayjeet Thakare <jeetthakare@hotmail.com>
2017-06-28 22:50:44 +0530

    changes

--- tfidf.py
+++ tfidf.py
@@ -1,1 +1,10 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
 import nltk
+
+que1 = "how can I go form Pune to Mumbai"
+que2 = "How is the weather in Pune today"
+
+ans1 = "Get a cab"
+ans2 = "Check Google Now"
+
+vect = TfidfVectorizer(stop_words='english', min_df=1)
