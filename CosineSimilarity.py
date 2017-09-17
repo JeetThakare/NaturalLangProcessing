012cca59ec47661263c3358b9ec088f6992dd55f
Pranayjeet Thakare <jeetthakare@hotmail.com>
2017-09-17 23:47:38 +0530

    checked similarity

--- CosineSimilarity.py
+++ CosineSimilarity.py
@@ -1,7 +1,10 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pprint
-tokenize = lambda doc: doc.lower().split(" ")
+
+
+def tokenize(doc): return doc.lower().split(" ")
+
 
 document_0 = "China has a strong economy that is growing at a rapid pace. However politically it differs greatly from the US Economy."
 document_1 = "At last, China seems serious about confronting an endemic problem: domestic violence and corruption."
@@ -11,16 +14,10 @@ document_4 = "What's the future of Abenomics? We asked Shinzo Abe for his views"
 document_5 = "Obama has eased sanctions on Cuba while accelerating those against the Russian Economy, even as the Ruble's value falls almost daily."
 document_6 = "Vladimir Putin is riding a horse while hunting deer. Vladimir Putin always seems so serious about things - even riding horses. Is he crazy?"
 
-all_documents = [document_0, document_1, document_2, document_3, document_4, document_5, document_6]
+all_documents = (document_0, document_1, document_2, document_3, document_4, document_5, document_6)
 
-sklearn_tfidf = TfidfVectorizer(min_df=0, use_idf=True, tokenizer=tokenize)
-sklearn_representation = sklearn_tfidf.fit_transform(all_documents)
+tfidf_vectorizer = TfidfVectorizer(use_idf=True)
+tfidf_matrix = tfidf_vectorizer.fit_transform(all_documents)
+print tfidf_matrix.shape
 
-query = "Putin is working to fix economic situation"
-
-
-query_tfidf = sklearn_tfidf.fit_transform([query])
-
-for _, q in enumerate(sklearn_representation.toarray()):
-    print(cosine_similarity(q,query_tfidf))
-    print('-------------------') #Not Working
+print(cosine_similarity(tfidf_matrix[0:1], tfidf_matrix))  # Similarity of first document with other
